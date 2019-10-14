25b831816e24bfd3e682a8bbd182640210921f13
Sabbir <sabbir.hossain133045@gmail.com>
2019-10-14 06:17:37 +0600

    Refrigerator

---
+++ RefrigeratorAppPractice3/Refrigerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    public class Refrigerator
+    {
+        private double capacity;
+        private double currentUsedCapacity;
+        private double remainingCapacity;
+
+        public Refrigerator( double capacity)
+        {
+            this.capacity = capacity;
+            this.remainingCapacity = capacity;
+        }
+        public bool store(double items, double weight)
+        {
+            bool flag = false;
+            double totalStoringAmount = items * weight;
+            if (remainingCapacity >= totalStoringAmount)
+            {
+                currentUsedCapacity += totalStoringAmount;
+                remainingCapacity -= totalStoringAmount;
+
+                flag = true;
+            }
+            return flag;
+        }
+        public double getCurrentUsedCapacity()
+        {
+            return currentUsedCapacity;
+        }
+        public double getRemainingCapacity()
+        {
+            return remainingCapacity;
+        }
+    }
+}
